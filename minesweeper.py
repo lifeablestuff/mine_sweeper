9551e46caf46e804872aa273a1fef5e21c54f8d8
Ian Shi <72175372+lifeablestuff@users.noreply.github.com>
2022-12-02 15:07:18 -0800

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -8,10 +8,16 @@ class mine(Fl_Window):
 		self.bl = []
 		self.bombs = []
 		self.numbered = {}
+		self.cords = []
+		self.flag = Fl_PNG_Image('flag.png').copy(50,50)
+		self.minepic=Fl_JPEG_Image('bomb.jpg').copy(50,50)
+		
 		for row in range(10):
 			for col in range(10):
 				self.bl.append(Fl_Button(row*50+50,col*50+50,50,50))
 				self.bl[-1].callback(self.button_click)
+				self.cords.append([row,col])
+				
 		
 		Fl.scheme('gtk+')
 		self.resizable(self)
@@ -27,31 +33,125 @@ class mine(Fl_Window):
 	def numbered_tiles(self):
 		bombs = 0
 		for x in range(len(self.bl)):
-			if self.bl[x] in bombs:
+			if self.bl[x] in self.bombs:
 				continue
 			else:
-				try:
-					if self.bl[x+1] in bombs:
-						bombs.append(self.bl[x+1])
-					if self.bl[x-1] in bombs:
-						bombs.append(self.bl[x-1])
-					if self.bl[x-11] in bombs:
-						bombs.append(self.bl[x-11])
-					if self.bl[x-10] in bombs:
-						bombs.append(self.bl[x-10])
-					if self.bl[x-9] in bombs:
-						bombs.append(self.bl[x-9])
-					if self.bl[x+9] in bombs:
-						bombs.append(self.bl[x+9])
-					if self.bl[x+10] in bombs:
-						bombs.append(self.bl[x+10])
-					if self.bl[x+11] in bombs:
-						bombs.append(self.bl[x+10])
-				except:
-					print('')
-			if len(bombs) == 0:
-				self.numbered[bl[x]] = 0
+				# right side
+				if self.cords[x][1] == 10:
+					# if top right
+					if self.cords[x][0] == 0:
+						if self.bl[x-1] in self.bombs:
+							bombs += 1
+						if self.bl[x+10] in self.bombs:
+							bombs += 1
+						if self.bl[x+9] in self.bombs:
+							bombs += 1
+					# if bottom right
+					elif self.cords[x][0] == 10:
+						if self.bl[x-1] in self.bombs:
+							bombs += 1
+						if self.bl[x-10] in self.bombs:
+							bombs += 1
+						if self.bl[x-11] in self.bombs:
+							bombs += 1
+					else:
+						if self.bl[x-1] in self.bombs:
+							bombs += 1
+						if self.bl[x-10] in self.bombs:
+							bombs += 1
+						if self.bl[x-9] in self.bombs:
+							bombs += 1
+						if self.bl[x+10] in self.bombs:
+							bombs += 1
+						if self.bl[x+9] in self.bombs:
+							bombs += 1
+				# top side
+				elif self.cords[x][0] == 0:
+					# if top left
+					if self.cords[x][1] == 0:
+						if self.bl[x+10] in self.bombs:
+							bombs += 1
+						if self.bl[x+11] in self.bombs:
+							bombs += 1
+						if self.bl[x+1] in self.bombs:
+							bombs += 1
+							
+					else:
+						if self.bl[x-1] in self.bombs:
+							bombs += 1
+						if self.bl[x+1] in self.bombs:
+							bombs += 1
+						if self.bl[x+9] in self.bombs:
+							bombs += 1
+						if self.bl[x+10] in self.bombs:
+							bombs += 1
+						if self.bl[x+11] in self.bombs:
+							bombs += 1
+				# left side
+				elif self.cords[x][1] == 0:
+					if self.cords[x][0] == 0:
+						if self.bl[x+10] in self.bombs:
+							bombs += 1
+						if self.bl[x+1] in self.bombs:
+							bombs += 1
+						if self.bl[x+11] in self.bombs:
+							bombs += 1
+					# if bottom left
+					elif self.cords[x][0] == 10:
+						if self.bl[x-10] in self.bombs:
+							bombs += 1
+						if self.bl[x-9] in self.bombs:
+							bombs += 1
+						if self.bl[x+1] in self.bombs:
+							bombs += 1
+					
+					else:
+						if self.bl[x-1] in self.bombs:
+							bombs += 1
+						if self.bl[x+1] in self.bombs:
+							bombs += 1
+						if self.bl[x+9] in self.bombs:
+							bombs += 1
+						if self.bl[x+10] in self.bombs:
+							bombs += 1
+						if self.bl[x+11] in self.bombs:
+							bombs += 1
+				
+				# bottom side
+				elif self.cords[x][0] == 10:
+					if self.bl[x+1] in self.bombs:
+						bombs += 1
+					if self.bl[x-1] in self.bombs:
+						bombs += 1
+					if self.bl[x-11] in self.bombs:
+						bombs += 1
+					if self.bl[x-10] in self.bombs:
+						bombs += 1
+					if self.bl[x-9] in self.bombs:
+						bombs += 1
+				
+				# if anywhere else in grid
+				else:
+					if self.bl[x+1] in self.bombs:
+						bombs += 1
+					if self.bl[x-1] in self.bombs:
+						bombs += 1
+					if self.bl[x-11] in self.bombs:
+						bombs += 1
+					if self.bl[x-10] in self.bombs:
+						bombs += 1
+					if self.bl[x-9] in self.bombs:
+						bombs += 1
+					if self.bl[x+9] in self.bombs:
+						bombs += 1
+					if self.bl[x+10] in self.bombs:
+						bombs += 1
+					if self.bl[x+11] in self.bombs:
+						bombs += 1
+		
+			self.numbered[x] = bombs
 		print(self.numbered)
+							
 		
 	
 	def button_click(self,wid):
