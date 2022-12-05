a3b5c69a37499806ca50cadf0333808bada1bbb1
Ian Shi <72175372+lifeablestuff@users.noreply.github.com>
2022-12-05 15:06:23 -0800

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -11,13 +11,13 @@ class mine(Fl_Window):
 		self.cords = []
 		self.flag = Fl_PNG_Image('flag.png').copy(50,50)
 		self.minepic=Fl_JPEG_Image('bomb.jpg').copy(50,50)
-		
+		num = 0
 		for row in range(10):
 			for col in range(10):
-				self.bl.append(Fl_Button(row*50+50,col*50+50,50,50))
+				self.bl.append(Fl_Button(col*50+50,row*50+50,50,50))
 				self.bl[-1].callback(self.button_click)
+				num += 1
 				self.cords.append([row,col])
-				
 		
 		Fl.scheme('gtk+')
 		self.resizable(self)
@@ -27,129 +27,25 @@ class mine(Fl_Window):
 	
 	def randomize_bombs(self):
 		for x in range(10):
-			a = random.randint(1,100)
-			self.bombs.append(self.bl[a])
+			a = random.randint(0,99)
+			self.bombs.append(self.cords[a])
+			self.bl[a].image(self.minepic)
+			self.redraw()
 		
 	def numbered_tiles(self):
 		bombs = 0
-		for x in range(len(self.bl)):
+		print(self.cords)
+		for x in range(len(self.cords)):
 			if self.bl[x] in self.bombs:
 				continue
-			else:
-				# right side
-				if self.cords[x][1] == 10:
-					# if top right
-					if self.cords[x][0] == 0:
-						if self.bl[x-1] in self.bombs:
-							bombs += 1
-						if self.bl[x+10] in self.bombs:
-							bombs += 1
-						if self.bl[x+9] in self.bombs:
-							bombs += 1
-					# if bottom right
-					elif self.cords[x][0] == 10:
-						if self.bl[x-1] in self.bombs:
-							bombs += 1
-						if self.bl[x-10] in self.bombs:
-							bombs += 1
-						if self.bl[x-11] in self.bombs:
-							bombs += 1
-					else:
-						if self.bl[x-1] in self.bombs:
-							bombs += 1
-						if self.bl[x-10] in self.bombs:
-							bombs += 1
-						if self.bl[x-9] in self.bombs:
-							bombs += 1
-						if self.bl[x+10] in self.bombs:
-							bombs += 1
-						if self.bl[x+9] in self.bombs:
-							bombs += 1
-				# top side
-				elif self.cords[x][0] == 0:
-					# if top left
-					if self.cords[x][1] == 0:
-						if self.bl[x+10] in self.bombs:
-							bombs += 1
-						if self.bl[x+11] in self.bombs:
-							bombs += 1
-						if self.bl[x+1] in self.bombs:
-							bombs += 1
+	
+			
 							
-					else:
-						if self.bl[x-1] in self.bombs:
-							bombs += 1
-						if self.bl[x+1] in self.bombs:
-							bombs += 1
-						if self.bl[x+9] in self.bombs:
-							bombs += 1
-						if self.bl[x+10] in self.bombs:
-							bombs += 1
-						if self.bl[x+11] in self.bombs:
-							bombs += 1
-				# left side
-				elif self.cords[x][1] == 0:
-					if self.cords[x][0] == 0:
-						if self.bl[x+10] in self.bombs:
-							bombs += 1
-						if self.bl[x+1] in self.bombs:
-							bombs += 1
-						if self.bl[x+11] in self.bombs:
-							bombs += 1
-					# if bottom left
-					elif self.cords[x][0] == 10:
-						if self.bl[x-10] in self.bombs:
-							bombs += 1
-						if self.bl[x-9] in self.bombs:
-							bombs += 1
-						if self.bl[x+1] in self.bombs:
-							bombs += 1
 					
-					else:
-						if self.bl[x-1] in self.bombs:
-							bombs += 1
-						if self.bl[x+1] in self.bombs:
-							bombs += 1
-						if self.bl[x+9] in self.bombs:
-							bombs += 1
-						if self.bl[x+10] in self.bombs:
-							bombs += 1
-						if self.bl[x+11] in self.bombs:
-							bombs += 1
-				
-				# bottom side
-				elif self.cords[x][0] == 10:
-					if self.bl[x+1] in self.bombs:
-						bombs += 1
-					if self.bl[x-1] in self.bombs:
-						bombs += 1
-					if self.bl[x-11] in self.bombs:
-						bombs += 1
-					if self.bl[x-10] in self.bombs:
-						bombs += 1
-					if self.bl[x-9] in self.bombs:
-						bombs += 1
-				
-				# if anywhere else in grid
-				else:
-					if self.bl[x+1] in self.bombs:
-						bombs += 1
-					if self.bl[x-1] in self.bombs:
-						bombs += 1
-					if self.bl[x-11] in self.bombs:
-						bombs += 1
-					if self.bl[x-10] in self.bombs:
-						bombs += 1
-					if self.bl[x-9] in self.bombs:
-						bombs += 1
-					if self.bl[x+9] in self.bombs:
-						bombs += 1
-					if self.bl[x+10] in self.bombs:
-						bombs += 1
-					if self.bl[x+11] in self.bombs:
-						bombs += 1
-		
+			
+			
 			self.numbered[x] = bombs
+		
 		print(self.numbered)
 							
 		
@@ -157,6 +53,12 @@ class mine(Fl_Window):
 	def button_click(self,wid):
 		if wid in self.bombs:
 			fl_message('you lose')
+		else:
+			if self.numbered.get(wid) != 0:
+				wid.label(str(self.numbered.get(self.bl.index(wid))))
+				print(self.cords[self.bl.index(wid)])
+				wid.deactivate()
+		
 		
 				
 app = mine(500,500,600,700,'game')
